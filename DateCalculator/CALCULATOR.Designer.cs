6f96a95ff80ad85c1e277053f4e4ac6ea9c2fc8b
gemma14 <gemmarosealacio@gmail.com>
2014-07-04 12:01:57 -0700

    Csharp
    2222

---
+++ DateCalculator/CALCULATOR.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace DateCalculator
+{
+    partial class CALCULATOR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(76, 84);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(271, 24);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(76, 39);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(271, 24);
+            this.dateTimePicker2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CONVERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CALCULATOR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 381);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "CALCULATOR";
+            this.Text = "CALCULATOR";
+            this.Load += new System.EventHandler(this.CALCULATOR_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ DateCalculator/CALCULATOR.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateCalculator
+{
+    public partial class CALCULATOR : Form
+    {
+        public CALCULATOR()
+        {
+            InitializeComponent();
+        }
+
+        private void CALCULATOR_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string date1 = dateTimePicker1.Text;
+            string date2 = dateTimePicker2.Text;
+            int counter = 0, month1 = 0, day1 = 0, year1 = 0, month2 = 0, day2 = 0, year2 = 0;
+            DateTime result1 = dateTimePicker1.Value;
+            DateTime result2 = dateTimePicker2.Value;
+            
+            foreach(string date in result1.ToString().Split(' ', ',', '/'))
+            {
+                if (counter == 3)
+                    break;
+                else if (counter == 0)
+                    month1 = int.Parse(date);
+                else if (counter == 1)
+                    day1 = int.Parse(date);
+                else if (counter == 2)
+                    year1 = int.Parse(date);
+                counter++;
+            }
+
+            counter = 0;
+            foreach (string date in result2.ToString().Split(' ', ',', '/'))
+            {
+                if (counter == 3)
+                    break;
+                else if (counter == 0)
+                    month2 = int.Parse(date);
+                else if (counter == 1)
+                    day2 = int.Parse(date);
+                else if (counter == 2)
+                    year2 = int.Parse(date);
+                counter++;
+            }
+            
+            try
+            {
+                DateTime oldDate = new DateTime(year1, month1, day1);
+                DateTime newDate = new DateTime(year2, month2, day2);
+                TimeSpan ts = newDate - oldDate;
+
+                double seconds = ts.TotalSeconds;
+                double minutes = seconds / 60;
+                double hours = minutes / 60;
+                double days = hours / 24;
+                int years = (int) days / 365;
+                int months = years * 12;
+
+                MessageBox.Show("\nYears: " + years.ToString() +
+                                "\nMonths: " + months.ToString() +
+                                "\nDays: " + days.ToString() +
+                                "\nHours: " + hours.ToString() +
+                                "\nMinutes: " + minutes.ToString() +
+                                "\nSeconds: " + seconds.ToString());
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+    }
+}
